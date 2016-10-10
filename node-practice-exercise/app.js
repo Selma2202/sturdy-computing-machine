4baae3a11ae91d072f45d9bf4cf3dee7a6506d9c
Selma Dorrestein <selmadorrestein@gmail.com>
2016-10-10 15:20:48 +0200

    finished class practice exercise

--- node-practice-exercise/app.js
+++ node-practice-exercise/app.js
@@ -1,3 +1,37 @@
 //setup project
 //read files from the directory
 //use that information to print out the files
+
+var fs = require ("fs")
+
+fs.readdir(__dirname, function(err, files){
+	if (err) throw err;
+	// console.log(files)
+
+	for (var i = 0; i < files.length; i++) {
+
+		fs.readFile(files[i], "utf8", function(err, data) {
+			if (err) throw err;
+			console.log(data);
+		})
+	}
+
+})
+
+
+// for (var i = Things.length - 1; i >= 0; i--) {
+// 	Things[i]
+// }
+
+
+
+// fs.readFile(files[i], "utf8", function(err, data) {
+// 	if (err) throw err;
+// 	console.log(data);
+// })
+
+
+// fs.readFile(__dirname + "fs.readdir"[1], "utf8", function(err, data) {
+// 	if (err) throw err;
+// 	console.log(data);
+// })
