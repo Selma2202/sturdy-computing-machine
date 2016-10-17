8ba1514fad5fc3253035f0b112c49dfab8828579
Selma Dorrestein <selmadorrestein@gmail.com>
2016-10-17 15:01:31 +0200

    all images and links work. finished homework

--- 1018-business-website-node/app.js
+++ 1018-business-website-node/app.js
@@ -3,7 +3,7 @@ const app = express ()
 
 console.log('Starting app')
 
-app.use('/', express.static('static/Jimmy-Selma-Coffee-Adventure'))
+app.use(express.static('static/Jimmy-Selma-Coffee-Adventure'))
 
 app.listen (3000, ()=>{
 	console.log('Express listening')
