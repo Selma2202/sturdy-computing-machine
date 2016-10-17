b8f7752d14dce5f8e06c37eeeedf61474ce70cc1
Selma Dorrestein <selmadorrestein@gmail.com>
2016-10-17 14:58:20 +0200

    testing 1

---
+++ 1018-business-website-node/app.js
@@ -0,0 +1,10 @@
+const express = require ( 'express')
+const app = express ()
+
+console.log('Starting app')
+
+app.use('/', express.static('static/Jimmy-Selma-Coffee-Adventure'))
+
+app.listen (3000, ()=>{
+	console.log('Express listening')
+})
