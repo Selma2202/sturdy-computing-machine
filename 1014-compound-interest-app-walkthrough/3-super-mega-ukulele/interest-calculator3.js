f979f1cf1233bb349e419223ea783770d6a79dae
Selma Dorrestein <selmadorrestein@gmail.com>
2016-10-17 17:02:45 +0200

    somehow finished exercise 3, super mega ukulele

---
+++ 1014-compound-interest-app-walkthrough/3-super-mega-ukulele/interest-calculator3.js
@@ -0,0 +1,92 @@
+"use strict" //anders leest ie niet in terminal de nieuwe termen
+
+// importing necessary modules
+const fs = require ( "fs")
+const path = require("path");
+
+
+
+//Voor nu ff alles wegcommenten wat nog niet nodig is
+
+
+
+//put everything needed to export in a function so it can get exported?
+let interestCalculator3 = () => {
+   
+
+var folder = (__dirname + "/customers")
+fs.readdir(folder, function (err, files) {
+    if (err) {
+        throw err;
+    }
+
+    files.map(function (file) {
+        return path.join(folder, file);
+    }).filter(function (file) {
+        return fs.statSync(file).isFile();
+    }).forEach(function (file) {
+		fs.readFile( file, "utf-8", ( err, data ) => {//new kind of function writing
+		//Parse the file to a readable object
+		let parsedData = JSON.parse(data)//let new datatype instead of var
+
+		//console.log (parsedData)
+		calcCompound ( parsedData )
+		//console.log (parsedData)
+		} )
+    });
+});
+
+
+
+
+// help function for later on
+let roundDecimal 	= (number) => {
+	return Math.round (number * 100 ) / 100
+}
+let addCommas 		= ( number ) => {
+	return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+let prettyNr 		= (number) => {
+	return addCommas(roundDecimal (number))
+}
+
+
+
+// Function to calculate compound interest from a customer object
+let calcCompound = ( customer ) => {
+	// Set end amount prop and calculate total duration
+	customer.pension.endamount = {
+		pessimistic: 	customer.finances.startcapital,
+		average: 		customer.finances.startcapital,
+		optimistic: 	customer.finances.startcapital
+	}
+	customer.pension.duration = (customer.pension.age - customer.age)
+
+	// Do the interest math
+	for (var i = customer.pension.duration - 1; i >= 0; i--) {//dit is the improved native for loop, efficienter omdat ie niet telkens een external object moet aanspreken om te checken of het nog moet blijven loopen.
+		//console.log ( "I looped " + i + " times")//dit is om te checken of de loop werkt
+
+		//Add monthly spend to all the scenarios
+		customer.pension.endamount.pessimistic 	+= (customer.finances.monthlyadd * 12 )
+		customer.pension.endamount.average 		+= (customer.finances.monthlyadd * 12 )
+		customer.pension.endamount.optimistic 	+= (customer.finances.monthlyadd * 12 )
+
+		//Calculate the added interest
+		customer.pension.endamount.pessimistic 	*= customer.pension.interest.pessimistic
+		customer.pension.endamount.average 		*= customer.pension.interest.average
+		customer.pension.endamount.optimistic 	*= customer.pension.interest.optimistic
+	}
+	//Welcome customer
+	console.log ("\nWelcome " + customer.name + " to our pension planner!")
+	console.log ("You are starting with " + customer.finances.startcapital + " and add a monthly amount of " + customer.finances.monthlyadd)
+	console.log ("When you retire at age " + customer.pension.age + " you will have the following:")
+	//Output data
+	console.log ("In a pessimistic scenario: €" 	+ prettyNr(customer.pension.endamount.pessimistic))
+	console.log ("In a average scenario: €" 		+ prettyNr(customer.pension.endamount.average))
+	console.log ("In a optimistic scenario: €" 		+ prettyNr(customer.pension.endamount.optimistic))
+}
+ }
+
+
+module.exports = interestCalculator3
--- 1014-compound-interest-app-walkthrough/assignmentsexplanation.md
+++ 1014-compound-interest-app-walkthrough/assignmentsexplanation.md
@@ -1,6 +1,6 @@
 Assignments Mentor: 
 
-# Super mini ukulele
+<!-- # Super mini ukulele
 - Modify the compound interest app to move the logic in a node module.
 Hints: module.exports
 
@@ -18,15 +18,30 @@ Steps:
 - check module for the times we have called upon the data and see if it needs changing.
 - it seems the module pretty much stays the same, except it has to happen x amount of times (as long as the array is long). In my case 5. When something needs to happen an x amount of time, we LOOP>
 - I now managed to get it to work for just one customer. Everywhere where it says [0], it might need to happen an x amount of time. 
-- So I need to get that part to loop. 
+- So I need to get that part to loop.  -->
 
 # SUPER MEGA UKULELE
 - Make a 'customers' folder with 10 customer json files (e.g. mentor.json, paul.json etc). Make your app read all files in this directory and write the results of the pension calculations to a 'customerprojections.json' file as an array of result objects.
 Hints: read folder, parse files, loop file and call calculations, save result variable to json file.
 
-# KING BOB
-- Make a generator that populates a process.argv[2] number of customer json files with random but reasonable data (meaning it could be real people you know, no trillionaires). Then use your super mega ukulele app to calculate their hypothetical pensions.
+STEPS:
+- make a customers folder
+- make 10 customer json files
+- make an app3.js
+- make an interest-calculator3.js. it should include:
+	- something to read the folder
+	- something to parse the files
+	- something to loop through files?
+	- or something that creates one big file?
+	- a variable 'result'
+	- the variable 'result' should be saved in a json file? or exported to a json file?
+NOTE AFTER MAKING THIS ASSIGNMENT
+- the only thing that needed to be different in the end, was to use readdir and parse files, with which google helped. After that, I could just use the complete old code from Assignment1/2 combined. sweet.
 
+
+<!-- # KING BOB
+- Make a generator that populates a process.argv[2] number of customer json files with random but reasonable data (meaning it could be real people you know, no trillionaires). Then use your super mega ukulele app to calculate their hypothetical pensions.
+ -->
 # Style bonus
 - use arrow functions ()=>{}
 - use not a single var statement
