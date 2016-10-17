081364b0bb0d6a82580ffdb4b081ed0c0660d58f
Selma Dorrestein <selmadorrestein@gmail.com>
2016-10-17 15:09:30 +0200

    added console.log for clarity

--- 1018-business-website-node/app.js
+++ 1018-business-website-node/app.js
@@ -4,4 +4,5 @@ const app = express ()
 app.use(express.static('static/Jimmy-Selma-Coffee-Adventure'))//put the whole folder in the static folder, so that had to be included in the path
 
 app.listen (3000, ()=>{//hosts it on localhost:3000
+	console.log("This app is running on localhost:3000")
 })
