f40cb70b21e0eaf70f8103699994298b1146f57f
Selma Dorrestein <selmadorrestein@gmail.com>
2016-10-18 14:07:39 +0200

    started setup for homework assignment

---
+++ 1024-user-information-app/app.js
@@ -0,0 +1,32 @@
+'use strict'
+
+//including necessary modules
+const express = require ('express')
+const fs = require ('fs')
+const app = express ()
+
+app.set ('view engine', 'pug')
+app.set ('views', __dirname + '/views')
+
+
+
+// Old example code
+// app.get ('/ping', ( request, response) => {
+// 	response.send ('Pong')
+// })
+
+// app.get ('/index', (request, response) => {
+// 	console.log( 'about to render a pug page woop woop')
+// 	fs.readFile( __dirname + '/data.json', (error, data) => {
+// 		if (error) throw error
+
+// 		let parsedData = JSON.parse(data)
+// 		console.log(parsedData)
+// 		response.render('index', {data: parsedData})
+// 	})
+	
+// })
+
+// app.listen (8000, () => {
+// 	console.log('server running')
+// })
