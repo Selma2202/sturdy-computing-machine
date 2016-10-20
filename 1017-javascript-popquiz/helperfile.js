2f529a283a9ca19d2f8f719e3d477b9eaf7253a3
Selma Dorrestein <selmadorrestein@gmail.com>
2016-10-20 13:56:55 +0200

    minor changes

---
+++ 1017-javascript-popquiz/helperfile.js
@@ -0,0 +1,20 @@
+var number = 12358
+var number = 334
+var number = 11
+var number = -134
+
+if (number > 1000) {
+	console.log ("This number is huge")
+} else if (number > 100) {
+	console.log ("This number is pretty big")
+} else {
+	console.log ("This number is small")
+}
+
+for (i=0, i < 10, i++) {
+	console.log ("I looped " + i + " times.")
+}
+
+for (var i = 0; i < Things.length; i++) {
+	Things[i]
+}
