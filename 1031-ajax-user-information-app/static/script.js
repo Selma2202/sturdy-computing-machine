5bcaea4cc954cc74bc9c565ccbcf4fd330371a3d
Selma Dorrestein <selmadorrestein@gmail.com>
2016-10-25 12:07:23 +0200

    added a script page, added scripts to pug, added basic get and post structure in script

---
+++ 1031-ajax-user-information-app/static/script.js
@@ -0,0 +1,10 @@
+$.get("/search", function(data, status) {
+	//process data
+	// the response will contain data that is present in the html
+})
+
+
+$.post ("/search", data, function(data, status){
+	//process data
+	//data as an object
+})
